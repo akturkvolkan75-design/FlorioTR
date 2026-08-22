84510132256911c53dbd17368097c9079ee3fd89
Mahmut <BURAYA_GITHUB_MAIL_ADRESIN>
2026-08-22 04:23:49 +0300

    Ana sayfaya arama kutusu eklendi

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
+import SearchBox from "@/components/SearchBox";
 import Categories from "@/components/home/Categories";
 import CustomerStories from "@/components/home/CustomerStories";
 import TrustCards from "@/components/home/TrustCards";
 import FeaturedFlowers from "@/components/home/FeaturedFlowers";
 
+
 export default function Home() {
   return (
     <main className="min-h-screen">
+
+
+      {/* ARAMA ALANI */}
 
       <section
         className="
@@ -15,6 +20,70 @@ export default function Home() {
           pt-6
         "
       >
+
+        <div
+          className="
+            rounded-[30px]
+            bg-white
+            p-8
+            text-center
+            shadow-lg
+          "
+        >
+
+          <h1
+            className="
+              text-3xl
+              font-black
+              text-[#123f34]
+            "
+          >
+            Çiçeğinizi kolayca bulun 🌸
+          </h1>
+
+
+          <p
+            className="
+              mt-2
+              text-gray-500
+            "
+          >
+            Çiçekçi veya konum arayın
+          </p>
+
+
+          <div
+            className="
+              mt-6
+              flex
+              justify-center
+            "
+          >
+
+            <SearchBox />
+
+          </div>
+
+
+        </div>
+
+      </section>
+
+
+
+
+
+      {/* KATEGORİLER + YORUMLAR */}
+
+      <section
+        className="
+          mx-auto
+          max-w-[1650px]
+          px-4
+          mt-5
+        "
+      >
+
         <div
           className="
             grid
@@ -23,12 +92,20 @@ export default function Home() {
             lg:items-stretch
           "
         >
+
           <Categories />
 
           <CustomerStories />
+
         </div>
+
       </section>
 
+
+
+
+
+      {/* POPÜLER ÇİÇEKLER */}
 
       <section
         className="
@@ -38,9 +115,16 @@ export default function Home() {
           px-4
         "
       >
+
         <FeaturedFlowers />
+
       </section>
 
+
+
+
+
+      {/* GÜVEN ALANI */}
 
       <section
         className="
@@ -50,8 +134,11 @@ export default function Home() {
           px-4
         "
       >
+
         <TrustCards />
+
       </section>
+
 
     </main>
   );
